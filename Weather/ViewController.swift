d38ab8a14df093a87c4ff89237199aa30cd38db7
Tilek <tilek@yurtah.com>
2021-09-21 19:48:44 +0600

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Tilek Sulaymanbekov on 21/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
